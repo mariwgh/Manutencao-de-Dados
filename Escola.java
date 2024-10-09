8e9f37694308119e14e13e5d9ccfe2c837def353
RafaellysSilva <162441877+RafaellysSilva@users.noreply.github.com>
2024-10-09 18:17:20 -0300

    tentativa de interface
    .

---
+++ Escola.java
@@ -0,0 +1,13 @@
+public interface Escola {
+    //inserir , excluir , consultar , alterar
+    incluir();
+    excluir();
+    alterar();
+    exibir();
+    mediaAritimetica();     //media por estudante e por disciplina
+    maiorNumeroAprovacoes();
+    maiorNumeroRetencoes();
+    getResul();     //para medias aritmeticas
+    
+
+}
