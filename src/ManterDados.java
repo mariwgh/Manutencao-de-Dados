89d94c320509b2e7308c34bb8b663507f0c37100
RafaellysSilva <162441877+RafaellysSilva@users.noreply.github.com>
2024-10-11 15:11:55 -0300

    .
    só copiei o que o chico já passou no pdf do projeto

---
+++ src/ManterDados.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.io.IOException;
+
+public interface ManterDados {
+        enum Situacao {navegando, incluindo, excluindo, alterando, editando,
+            buscando
+        };
+        void leituraDosDados(String nomeArquivo);
+        void gravarDados(String nomeArquivo) throws IOException;
+        Boolean existe(Estudante dadoProcurado); // pesquisa binária
+        void incluirNoFinal(Estudante novoDado);
+        void incluirEm(Estudante novoDado, int posicaoDeInclusao);
+        void excluir(int posicaoDeExclusao);
+        Estudante valorDe(int indiceDeAcesso);
+        void alterar(int posicaoDeAlteracao, Estudante novoDado);
+        void trocar(int origem, int destino);
+        void ordenar();
+        Boolean estaVazio();
+        Boolean estaNoInicio();
+        Boolean estaNoFim();
+        void irAoInicio(); // posicaoAtual fica no início do vetor
+        void irAoFim(); // posiçãoAtual fica no fim do vetor
+        void irAoAnterior(); // posiçãoAtual retrocede à posição anterior
+        void irAoProximo();
+        Situacao getSituacao();
+        void setSituacao(Situacao novaSituacao);
+}
---
+++ src/ManterEstudantes.java
@@ -0,0 +1,115 @@
+package src;
+
+import java.io.*;
+import static java.lang.System.out;
+
+public class ManterEstudantes implements ManterDados {
+    int qtosDados,
+            posicaoAtual;
+    Estudante[] dados;
+    Situacao situacao;
+    public void leituraDosDados(String nomeArquivo) {
+        try {
+            posicaoAtual = 0;
+            BufferedReader arquivoDeEntrada = new BufferedReader(
+                    new FileReader("c:\\temp\\dadosEstudantes.txt"));
+            String linhaDoArquivo = "";
+            try {
+                boolean parar = false;
+                while (! parar) {
+                    Estudante novoDado = new Estudante();
+
+                    try {
+
+                        if (novoDado.leuLinhaDoArquivo(arquivoDeEntrada) ) {
+                            incluirNoFinal(novoDado);
+                        }
+                        else
+
+                            parar = true;
+                    }
+
+                    catch (Exception erroDeLeitura) {
+
+                        out.println(erroDeLeitura.getMessage());
+
+                        parar = true;
+
+                    }
+                }
+                arquivoDeEntrada.close();
+            }
+            catch (IOException erroDeIO) {
+                out.println(erroDeIO.getMessage());
+            }
+        }
+        catch (FileNotFoundException erro) {
+            out.println(erro.getMessage());
+        }
+    }
+    public void gravarDados(String nomeArquivo) throws IOException {
+        BufferedWriter arquivoDeSaida = new BufferedWriter(
+                new FileWriter("c:\\temp\\dadosEstudantes.txt"));
+        for (int indice=0; indice < qtosDados; indice++)
+            arquivoDeSaida.write(dados[indice].formatoDeArquivo());
+        arquivoDeSaida.close();
+    }
+
+    public Boolean existe(Estudante dadoProcurado) {
+        return false;
+    }
+    public void incluirNoFinal(Estudante novoDado) {
+
+    }
+    public void incluirEm(Estudante novoDado, int posicaoDeInclusao) {
+
+    }
+    public void excluir(int posicaoDeExclusao) {
+
+    }
+    public Estudante valorDe(int indiceDeAcesso) {
+        return new Estudante();
+    }
+    public void alterar(int posicaoDeAlteracao, Estudante novoDado) {
+
+    }
+    public void trocar(int origem, int destino) {
+
+    }
+    public void ordenar() {
+
+    }
+    public Boolean estaVazio() {
+        return false;
+    }
+    public Boolean estaNoInicio() {
+        return false;
+    }
+    public Boolean estaNoFim() {
+        return false;
+    }
+    public void irAoInicio() {
+
+    }
+    public void irAoFim() {
+
+    }
+    public void irAoAnterior() {
+
+    }
+    public void irAoProximo() {
+
+    }
+    public int getPosicaoAtual() {
+        return 1;
+    }
+    public void setPosicaoAtual(int novaPosicao) {
+
+    }
+    public Situacao getSituacao() {
+        return situacao;
+    }
+    public void setSituacao(Situacao novaSituacao) {
+
+    }
+}
